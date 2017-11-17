3c768a3fb6618611de703d55dba4b90f23b7b13b
kcvisic <kcvisic@gmail.com>
2017-11-17 14:11:10 -0500

    Added connection settings and try catch

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,34 @@
 var mysql = require("mysql");
-var credendtials = require('credentials.js');
+var credendtials = null;
+
+// this file will only exist if we are running locally. If we run on heroku, our
+// db configutation will be set in the environment variables which we read below
+try {
+    credendtials = require('credentials.js');
+} catch (err) {
+    credendtials = {
+        HOST: "",
+        MYSQL_USER: "",
+        MYSQL_PASS: "",
+        MYSQL_DATABASE: "",
+    }
+
+}
+
+
 var connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST || credendtials.HOST,
-  user: process.env.MYSQL_USER || credendtials.MYSQL_USER,
-  password: process.env.MYSQL_PASS || credendtials.MYSQL_PASS,
-  database: process.env.MYSQL_DATABASE || credendtials.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST || credendtials.HOST,
+    user: process.env.MYSQL_USER || credendtials.MYSQL_USER,
+    password: process.env.MYSQL_PASS || credendtials.MYSQL_PASS,
+    database: process.env.MYSQL_DATABASE || credendtials.MYSQL_DATABASE,
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
